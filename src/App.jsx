b86d60db2ffe4ec6761c1365467e19c508798bf9
christian-bromann <mail@christian-bromann.com>
2019-07-11 15:09:36 +0200

    revert performance change

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const theme = createMuiTheme({
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.mineBitcoin(5000);
+    // this.mineBitcoin(5000);
   }
   mineBitcoin(duration) {
     const start = new Date().getTime();
